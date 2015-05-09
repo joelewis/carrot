9b133c9ce0c1823cc222586afdbb5c8618bb87b8
Joe Lewis <lewis.joe.18@gmail.com>
2015-05-09 11:06:36 +0530

    bootstrapped angularjs

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('carrot', [
+  'ngRoute',
+  'carrot.directives',
+  'carrot.controllers',
+
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: '/static/partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/apps/:app_id', {templateUrl: '/static/partials/app.html', controller: 'AppCtrl'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]).config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true).hashPrefix('!');
+}]);

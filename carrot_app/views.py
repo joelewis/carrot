223eda7260878a3ff7454d120e97699ce0f67296
Wafflespeanut <wafflespeanut@gmail.com>
2015-05-09 12:12:45 +0530

    login's still a problem...

--- carrot_app/views.py
+++ carrot_app/views.py
@@ -1,21 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import models, authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def user_signup(request):
     if request.method == 'POST':
-        user = request.POST['user']; passw = request.POST['passw']
+        user = request.POST['user']
+        passw = request.POST['passw']
         email = request.POST['email']
-        user_obj = models.User.objects.create_user(user, email, passw)
+        user_obj = User.objects.create_user(user, email, passw)
         user_obj.save()
+        user_obj = authenticate(user, passw)
         login(request, user_obj)
         return HttpResponseRedirect('/')
     return render(request, 'register.html', {});
 
+@csrf_exempt
 def user_login(request):
     if request.method == 'POST':
-        user = request.POST['user']; passw = request.POST['passw']
-        user_obj = authenticate(user,passw)
+        user = request.POST['user']
+        passw = request.POST['passw']
+        user_obj = authenticate(user, passw)
         if user is not None:
             login(request, user_obj)
             return HttpResponseRedirect('/')

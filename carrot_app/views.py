85d8bab24d27c40f9b09816af003e9bcd4daf570
Joe Lewis <lewis.joe.18@gmail.com>
2015-05-09 11:59:39 +0530

    data model

--- carrot_app/views.py
+++ carrot_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
 def user_login(request):
     with open('login.html','r') as file: data=file.readlines()
     return HttpResponse(''.join(data))
